9aa5439c17d15c9c16d68a24328f3cb67c9aff14
JinMyeong Choi <ymo6538@gmail.com>
2026-03-27 15:22:33 +0900

    feat: Mock 배포용 인증 안내 UX 개선

--- src/constants/mockAuth.ts
+++ src/constants/mockAuth.ts
@@ -15,3 +15,17 @@ export const MOCK_LOGIN_USER: User = {
   gender: 'M',
   profile_img_url: null,
 }
+
+export const MOCK_EMAIL_VERIFICATION_CODE = 'ABCDE12345'
+export const MOCK_SMS_VERIFICATION_CODE = '123456'
+
+export const MOCK_SIGNUP_EXAMPLE = {
+  name: '홍길동',
+  birthdate: '20000101',
+  nickname: '체험유저01',
+  email: 'demo01@example.com',
+  phoneNumber: '010-9876-5432',
+}
+
+export const MOCK_SOCIAL_LOGIN_TOAST_MESSAGE =
+  '목 모드에서는 소셜 로그인을 지원하지 않습니다. 일반 로그인을 사용해주세요.'
